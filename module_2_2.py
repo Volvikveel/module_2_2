946c8a84e15f2056bcdf4477d548734de4085b63
Volvik <volvikveel@gmail.com>
2024-11-28 16:02:44 +0300

    module_2_2

---
+++ module_2_2.py
@@ -0,0 +1,11 @@
+# Ввоодим цифры
+first = int(input())
+second = int(input())
+third = int(input())
+
+if first == second == third:
+    print(3)
+elif (first == second) or (first == third) or (second == third):
+    print(2)
+else:
+    print(0)
